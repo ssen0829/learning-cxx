14c87424886f95cbf4523ee974122475c19108cb
夏成禹 <1879530196@qq.com>
2026-05-08 21:22:47 +0800

    init: exercises 0-33

---
+++ exercises/08_pointer/main.cpp
@@ -0,0 +1,44 @@
+﻿#include "../exercise.h"
+
+// READ: 数组向指针退化 <https://zh.cppreference.com/w/cpp/language/array#%E6%95%B0%E7%BB%84%E5%88%B0%E6%8C%87%E9%92%88%E7%9A%84%E9%80%80%E5%8C%96>
+bool is_fibonacci(int *ptr, int len, int stride) {
+    ASSERT(len >= 3, "`len` should be at least 3");
+
+    // 遍历所有需要检查的三元组
+    for (int i = 0; i < len - 2; ++i) {
+        // 计算当前子序列第 i、i+1、i+2 个元素在原数组中的位置
+        int idx1 = i * stride;
+        int idx2 = (i + 1) * stride;
+        int idx3 = (i + 2) * stride;
+
+        int a = ptr[idx1];
+        int b = ptr[idx2];
+        int c = ptr[idx3];	
+
+        // 不满足斐波那契条件，直接返回 false
+        if (c != a + b) {
+            return false;
+        }
+    }
+
+    // 所有检查都通过
+    return true;
+}
+// ---- 不要修改以下代码 ----
+int main(int argc, char **argv) {
+    int arr0[]{0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55},
+        arr1[]{0, 1, 2, 3, 4, 5, 6},
+        arr2[]{99, 98, 4, 1, 7, 2, 11, 3, 18, 5, 29, 8, 47, 13, 76, 21, 123, 34, 199, 55, 322, 0, 0};
+    // clang-format off
+    ASSERT( is_fibonacci(arr0    , sizeof(arr0) / sizeof(*arr0)    , 1),         "arr0 is Fibonacci"    );
+    ASSERT( is_fibonacci(arr0 + 2, sizeof(arr0) / sizeof(*arr0) - 4, 1), "part of arr0 is Fibonacci"    );
+    ASSERT(!is_fibonacci(arr1    , sizeof(arr1) / sizeof(*arr1)    , 1),         "arr1 is not Fibonacci");
+    ASSERT( is_fibonacci(arr1 + 1,  3                              , 1), "part of arr1 is Fibonacci"    );
+    ASSERT(!is_fibonacci(arr2    , sizeof(arr2) / sizeof(*arr2)    , 1),         "arr2 is not Fibonacci");
+    ASSERT( is_fibonacci(arr2 + 2, 10                              , 2), "part of arr2 is Fibonacci"    );
+    ASSERT( is_fibonacci(arr2 + 3,  9                              , 2), "part of arr2 is Fibonacci"    );
+    ASSERT(!is_fibonacci(arr2 + 3, 10                              , 2), "guard check"                  );
+    ASSERT(!is_fibonacci(arr2 + 1, 10                              , 2), "guard check"                  );
+    // clang-format on
+    return 0;
+}
---
+++ exercises/11_method/main.cpp
@@ -0,0 +1,34 @@
+#include "../exercise.h"
+
+struct Fibonacci {
+    unsigned long long cache[128];
+    int cached;
+
+    // TODO: 实现正确的缓存优化斐波那契计算
+     unsigned long long get(int i) {
+        // 初始化 0 和 1 项
+        if (cached == 0) {
+            cache[0] = 0;
+            cached++;
+        }
+        if (cached == 1) {
+            cache[1] = 1;
+            cached++;
+        }
+
+        // 循环计算到 i
+        for (; cached <= i; ++cached) {
+            cache[cached] = cache[cached - 1] + cache[cached - 2];
+        }
+
+        return cache[i];
+    }
+};
+
+int main(int argc, char **argv) {
+    // TODO: 初始化缓存结构体，使计算正确
+    Fibonacci fib{};
+    ASSERT(fib.get(10) == 55, "fibonacci(10) should be 55");
+    std::cout << "fibonacci(10) = " << fib.get(10) << std::endl;
+    return 0;
+}
